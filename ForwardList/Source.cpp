616fb34a1045c78be13d694b24936d8480af4c42
yandutka@mail.ru <yandutka@mail.ru>
2021-12-09 21:44:13 +0300

    commit

--- ForwardList/Source.cpp
+++ ForwardList/Source.cpp
@@ -11,17 +11,22 @@ class Element
 {
 	int Data;          //Значение элемента
 	Element* pNext;    //Адрес следующего элемента
+	static int count;  //Количество элементов
 public:
 	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
 	{
+		count++;
 		cout << "EConstructor:\t" << this << endl;
 	}
 	~Element()
 	{
+		count--;
 		cout << "EDestructor:\t" << this << endl;
 	}
 	friend class ForwardList;
 };
+
+int Element::count = 0;    //Статические переменные могут быть проинициализированны только за классом
 
 class ForwardList
 {
@@ -30,11 +35,11 @@ public:
 	ForwardList()
 	{
 		this->Head = nullptr; //Если голова указывает на 0, значит список пуст
-		cout << "LConstructor:\t" << this << endl;
+		cout << "FLConstructor:\t" << this << endl;
 	}
 	~ForwardList()
 	{
-		cout << "LDestructor:\t" << this << endl;
+		cout << "FLDestructor:\t" << this << endl;
 	}
 
 	//           Adding elements:
@@ -50,52 +55,61 @@ public:
 
 	void push_back(int Data)
 	{
+		if (Head == nullptr)return push_front(Data);
 		Element* New = new Element(Data);
 		Element* Temp = Head;
-		while (true)
+		while (Temp->pNext)
 		{
-			if (Temp->pNext == nullptr)break;
 			Temp = Temp->pNext;
 		}
 		Temp->pNext = New;
 	}
 
+	//             Erasing elements:
 	void pop_front()
 	{
-		Element* Temp = Head->pNext;  //Присваеваем итератору указатель на 2 элемент
-		delete Head;                  //Удаляем Голову
-		Head = Temp;                  //Назначаем новый головной элемент
+		/*Element* Temp = Head->pNext;  //Присваеваем итератору указатель на 2 элемент
+		delete Head;                    //Удаляем Голову
+		Head = Temp;                    //Назначаем новый головной элемент*/
+		//1)Запоминаем адрес удаляемого элемента:
+		Element* Erased = Head;
+		//2)Исключаем элемент из списка:
+		Head = Head->pNext;
+		//3)Удаляем элемент из списка:
+		delete Erased;
 	}
 
 	void pop_back()
 	{
-		Element* Temp = Head;          
-		while (Temp->pNext->pNext!=nullptr)Temp = Temp->pNext;
+		if (Head->pNext == nullptr)return pop_front();
+		//1)Доходим до предпоследнего элемента:
+		Element* Temp = Head;                                                          
+		while (Temp->pNext->pNext)Temp = Temp->pNext;   //Мы оказываемся в предпоследнем элементе
+		//2)Удаляем последний элемент из памяти:
 		delete Temp->pNext;
+		//3)"Забываем" об удалённом элементе, то есть затираемего адрес нулём:
 		Temp->pNext = nullptr;
+		//Теперь Temp является последним элементом списка. 
 	}
 
-	void insert(int Data, int n)
+	void insert(int Data, int Index)
 	{
-		Element* Temp = Head;
-		int i = 0;
-		while (Temp != nullptr)
+		if (Index > Head->count)
 		{
-			Temp = Temp->pNext;
-			i++;
-		}
-		if (n > i)
-		{
-			cout << "Вы вышли за пределы списка!" << endl;
+			cout << "Error 404: Выход за пределы списка!" << endl;
 			return;
 		}
-		else
-		{
-			Element* New = new Element(Data);
-			New->pNext = Temp->pNext;
-			Temp->pNext = New;
-		}
-
+		if (Index == 0 || Head == nullptr)return push_front(Data);
+		//0)Доходим до нужного элемента:
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+		//1)Создаём новый элемент:
+		Element* New = new Element(Data);
+		//Осуществляем вставку нового элемента в список:
+		//2)Привязываем новый элемент к списку:
+		New->pNext = Temp->pNext;
+		//3)Включаем элемент в список:
+		Temp->pNext = New;
 	}
 
 	//           Methods:
@@ -109,6 +123,7 @@ public:
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;  //Переход на следующий элемент
 		}
+		cout << "Количество элементов списка: " << Head->count << endl;
 	}
 };
 
@@ -120,10 +135,10 @@ void main()
 	ForwardList list;
 	for (int i = 0; i < n; i++)
 	{
-		list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
 	}
 	list.Print();
-	cout << delimiter << endl;
+	/*cout << delimiter << endl;
 	list.push_back(8);
 	list.Print();
 	cout << delimiter << endl;
@@ -132,7 +147,15 @@ void main()
 	cout << delimiter << endl;
 	list.pop_back();
 	list.Print();
-	cout << delimiter << endl;
-	list.insert(100, 3);
+	cout << delimiter << endl;*/
+	int index, value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(value, index);
 	list.Print();
+
+	/*list.push_back(123);
+	list.Print();
+	list.pop_back();
+	list.Print();*/
 }
